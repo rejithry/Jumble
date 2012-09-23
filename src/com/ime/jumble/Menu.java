0c6bb5a3ef013d9dd22129a4282d461dd2ea6c44
U-Rejith-PC\Rejith <Rejith@Rejith-PC.(none)>
2012-09-23 15:39:08 -0700

    Changed Menu.java

--- src/com/ime/jumble/Menu.java
+++ src/com/ime/jumble/Menu.java
@@ -1,16 +1,3 @@
-/* 
- * Copyright (C) 2010 iMe Technologies Ltd.
- * Licensed under the GPL 
- *
- * Author : Rejith
- * Version 1.0
- *
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
- */
 package com.ime.jumble;
 
 import android.app.ListActivity;
